002577d6402c03b0c4c40f8465a8ffe5a72a21ab
Sebastian Michaelsen <sebastian@michaelsen.io>
2020-12-16 21:03:41 +0100

    [TASK] Remove include of cObj class
    That's what we have autoloading for.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,5 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-require_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('folder_cobj') . 'Classes/ContentObject/FolderContentObject.php');
-
 // Register new content objects
 $GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects']['FOLDER'] = \Smichaelsen\FolderCobj\ContentObject\FolderContentObject::class;
