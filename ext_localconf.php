5b002620f765f4467c9d4ecc8fd19d58ddeb77c4
Sebastian Michaelsen <sebastian@michaelsen.io>
2023-12-18 09:25:21 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') or die();
+defined('TYPO3') or die();
 
 // Register new content objects
 $GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects']['FOLDER'] = \Smichaelsen\FolderCobj\ContentObject\FolderContentObject::class;
